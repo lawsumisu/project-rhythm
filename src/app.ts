c14ab5ec8537c95174e72f4a39b21d8292a78b19
Law Smith <lawsumisu@gmail.com>
2025-08-01 03:26:34 +0900

    spike: added tone library, press button to beat logic

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,32 @@
 import { DebugDrawPlugin } from '@lawsumisu/phaser';
 import * as Phaser from 'phaser';
-import { Conductor } from 'src/conductor';
+import { Conductor, Metronome } from 'src/conductor';
+import * as Tone from 'tone';
+import { Vector2 } from '@lawsumisu/common-utilities';
 
 const keyCodes = Phaser.Input.Keyboard.KeyCodes;
+type PhaserSound = Phaser.Sound.HTML5AudioSound | Phaser.Sound.NoAudioSound | Phaser.Sound.WebAudioSound;
 
 class Scene extends Phaser.Scene {
   private conductor: Conductor;
-  private fillColor = 0;
+  private fillColor: number[] = [1, 1, 1];
+  private lastPress = -1;
+  private metronome: Metronome;
+  private synth = new Tone.Synth().toDestination();
+  private synth2 = new Tone.Synth().toDestination();
+  private beats: { position: Vector2; state?: string; timer: number }[] = [];
 
   public preload(): void {
     this.load.audio('theme', 'assets/audio/cryptic_marble.ogg');
   }
   public create(): void {
     const music = this.sound.add('theme');
-    this.conductor = new Conductor(music, 134, 0);
+    const bpm = 133;
+    this.conductor = new Conductor(music, bpm, 0);
+    this.metronome = new Metronome(bpm);
+    // transport.start();
+    this.metronome.start();
+    Tone.getContext().lookAhead = 0;
 
     this.input.on('pointerup', () => {
       if (music.isPlaying) {
@@ -22,21 +35,111 @@ class Scene extends Phaser.Scene {
         music.resume();
       }
     });
+
+    // Keyboard setup
+    [keyCodes.ENTER, keyCodes.A, keyCodes.UP, keyCodes.DOWN].forEach((k: number) => this.input.keyboard!.addKey(k));
   }
 
   public update(): void {
-    if (this.input.keyboard!.addKey(keyCodes.A).isDown) {
+    const {
+      [keyCodes.A]: A,
+      [keyCodes.ENTER]: ENTER,
+      [keyCodes.DOWN]: DOWN,
+      [keyCodes.UP]: UP,
+    } = this.input.keyboard!.keys;
+    const { bpm } = this.metronome;
+    const FPS = 60;
+    const initialTimer = (FPS * 60) / bpm;
+    if (A.isDown) {
       this.conductor.sound.play();
       this.conductor.reset();
     }
     this.conductor.update(() => {
-      this.fillColor = parseInt(
-        '0x' + [Math.random(), Math.random(), Math.random()].map((i) => Math.ceil(i * 255).toString(16)).join(''),
-        16
-      );
+      this.fillColor = [0, 0.5, 1];
     });
-    this.debugDraw.rect({ x: 50, y: 100, width: 60, height: 60 }, { fill: { color: this.fillColor }, lineWidth: 3 });
-    this.debugDraw.circle({ x: 50, y: 50, r: 15 }, { fill: { color: this.fillColor } });
+    this.metronome.update((beatIndex) => {
+      this.beats.push({
+        position: new Vector2(350, 50),
+        timer: initialTimer,
+        state: beatIndex % 4 === 1 ? '' : 'pass',
+      });
+      // this.synth.triggerAttackRelease('C2', '16n');
+    });
+    if (Phaser.Input.Keyboard.JustDown(ENTER) && this.lastPress === -1 && this.beats.length >= 2) {
+      // this.lastPress = this.sound.get<PhaserSound>('theme').seek;
+      const offset = this.metronome.getOffsetFromClosestBeat(Date.now() / 1000);
+      const targetBeat = this.beats
+        .filter((beat) => !beat.state)
+        .reduce((prev, beat) => {
+          return !prev || Math.abs(beat.timer) < Math.abs(prev.timer) ? beat : prev;
+        }, null);
+      if (targetBeat) {
+        if (Math.abs(offset) <= 1) {
+          targetBeat.state = 'perfect';
+          this.synth2.triggerAttackRelease('F4', '16n');
+        } else if (Math.abs(offset) <= 3) {
+          targetBeat.state = 'good';
+          this.synth.triggerAttackRelease('F3', '16n');
+        } else {
+          targetBeat.state = 'bad';
+          this.synth.triggerAttackRelease('F2', '16n');
+        }
+      }
+      console.log(this.metronome.getOffsetFromClosestBeat(Date.now() / 1000));
+    }
+    const color = parseInt(
+      '0x' +
+        this.fillColor
+          .map((i) =>
+            Math.ceil(i * 255)
+              .toString(16)
+              .padStart(2, '0')
+          )
+          .join(''),
+      16
+    );
+    this.beats.forEach((beat, i) => {
+      this.beats[i].timer -= 1;
+      let color;
+      switch (beat.state) {
+        case 'bad':
+          color = 0xff0000;
+          break;
+        case 'good':
+          color = 0x0000ff;
+          break;
+        case 'perfect':
+          color = 0x00ff00;
+          break;
+        default:
+          color = 0xffffff;
+          break;
+      }
+      this.debugDraw.circle(
+        { x: beat.position.x, y: beat.position.y + (initialTimer - beat.timer) * 3, r: beat.state === 'pass' ? 5 : 15 },
+        { fill: { color } }
+      );
+      beat.state !== 'pass' &&
+        this.debugDraw.circle(
+          {
+            x: beat.position.x,
+            y: beat.position.y + (initialTimer - beat.timer) * 3,
+            r: Math.max(((75 - 15) / initialTimer) * beat.timer + 15, 15),
+          },
+          { lineWidth: 3 }
+        );
+    });
+    this.beats = this.beats.filter((b) => b.timer >= -100);
+    this.debugDraw.line(0, 3 * initialTimer + 50, 800, 3 * initialTimer + 50, {});
+    this.debugDraw.rect({ x: 50, y: 100, width: 60, height: 60 }, { fill: { color }, lineWidth: 3 });
+    this.debugDraw.circle({ x: 50, y: 50, r: 15 }, { fill: { color } });
+    this.fillColor = this.fillColor.map((i) => Math.max(i - 0.01, 0.01));
+
+    if (Phaser.Input.Keyboard.JustDown(UP)) {
+      this.metronome.bpm += 5;
+    } else if (Phaser.Input.Keyboard.JustDown(DOWN)) {
+      this.metronome.bpm -= 5;
+    }
   }
 
   public get debugDraw(): DebugDrawPlugin {
@@ -44,7 +147,7 @@ class Scene extends Phaser.Scene {
   }
 }
 const gameConfig: Phaser.Types.Core.GameConfig = {
-  type: Phaser.WEBGL,
+  type: Phaser.CANVAS,
   // parent: 'phaser-example',
   width: 800,
   height: 600,
--- src/conductor.ts
+++ src/conductor.ts
@@ -1,3 +1,52 @@
+export class Metronome {
+  private startTime = 0;
+  private lastBeatUpdate = 0;
+  private _bpm: number;
+
+  constructor(bpm: number) {
+    this._bpm = bpm;
+  }
+
+  public start(): void {
+    this.startTime = Date.now() / 1000;
+    this.lastBeatUpdate = 0;
+  }
+
+  public update(fn: (beatCount: number) => void) {
+    const { _bpm, startTime, lastBeatUpdate } = this;
+    const secPerBeat = 60 / _bpm;
+    const elaspedTime = Date.now() / 1000 - startTime;
+    const beatIndex = Math.floor(elaspedTime / secPerBeat);
+    if (beatIndex != lastBeatUpdate) {
+      this.lastBeatUpdate = beatIndex;
+      fn(beatIndex);
+    }
+  }
+
+  public getOffsetFromClosestBeat(t: number, interval = 4) {
+    // Beatmap is action per quarter note
+    const { _bpm, startTime } = this;
+    const secPerBeat = 60 / _bpm;
+    const _t = t - startTime;
+    const beatIndex = Math.floor((_t / secPerBeat / interval) * 4);
+    if (_t - beatIndex * secPerBeat >= secPerBeat / 2) {
+      return Math.round((_t - (beatIndex + 1) * secPerBeat) * 60);
+    } else {
+      return Math.round((_t - beatIndex * secPerBeat) * 60);
+    }
+  }
+
+  public set bpm(_bpm: number) {
+    this._bpm = _bpm;
+    const secPerBeat = 60 / _bpm;
+    const elaspedTime = Date.now() / 1000 - this.startTime;
+    this.lastBeatUpdate = Math.floor(elaspedTime / secPerBeat);
+  }
+
+  public get bpm() {
+    return this._bpm;
+  }
+}
 export class Conductor {
   private readonly dt: number;
   private beatCount = 0;
@@ -17,9 +66,20 @@ export class Conductor {
 
   public update(fn: (beatCount: number) => void) {
     const { sound, dt } = this;
-    if (sound.isPlaying && sound.seek > dt + this.beatCount * dt) {
+    if (sound.isPlaying && sound.seek > (this.beatCount + 1) * dt) {
       this.beatCount++;
       fn(this.beatCount);
+    }
+  }
+
+  public getOffsetFromClosestBeat(t: number, f = 5) {
+    // Beatmap is action per quarter note
+    const { dt, beatCount } = this;
+    console.log(t - beatCount * dt, dt / 2);
+    if (t - beatCount * dt >= dt / 2) {
+      return Math.round((t - (beatCount + 1) * dt) * 60);
+    } else {
+      return Math.round((t - beatCount * dt) * 60);
     }
   }
 
